35eade6d08596d8edab4d5695838c816ddc6e35d
Marat Saubanov <supper_mario@bk.ru>
2019-08-15 20:20:57 +0500

    Max number

--- chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/MaxTest.java
@@ -5,7 +5,7 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 public class MaxTest {
     @Test
-    public void whenMax1To2Then2() {
+    public void SeconGreaterThenFirst() {
         Max max = new Max();
         int result = max.max(2, 3);
         assertThat(result, is(3));
