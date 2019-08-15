9bf230c0b01086f88859edcd55840f75339b556a
Marat Saubanov <supper_mario@bk.ru>
2019-08-15 23:05:27 +0500

    Factorial

---
+++ chapter_001/src/main/java/ru/job4j/loop/Factorial.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Factorial {
+    public int calc(int n) {
+        int result=1;
+        if(n>1){
+            for (int x=1;x<=n;++x){
+                result=x*result;
+            }
+        }
+        return result;
+    }
+}
