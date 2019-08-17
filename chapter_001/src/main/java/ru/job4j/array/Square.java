c1e72376c05bf2c87f95008d136d879b7d8899bb
Marat Saubanov <supper_mario@bk.ru>
2019-08-17 23:14:49 +0500

    Square array

--- chapter_001/src/main/java/ru/job4j/array/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/Square.java
@@ -1,6 +1,18 @@
 package ru.job4j.array;
 
+/**
+ * Class Класс для заполнения массива квадратами значений.
+ * @author msaubanov
+ * since 17.08.2019
+ * @version 1
+ */
 public class Square {
+    /**
+     * Сложение
+     * @param bound верхняя граница.
+     * @return массив квадратов чисел
+     */
+
     public int[] calculate(int bound) {
         int[] rst = new int[bound];
         // заполнить массив через цикл элементами от 1 до bound возведенными в квадрат
