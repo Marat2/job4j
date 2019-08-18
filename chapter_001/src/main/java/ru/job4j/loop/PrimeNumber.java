4a8639e8fa12a37120e836d2d707d6ea2d6ecca9
Marat Saubanov <supper_mario@bk.ru>
2019-08-18 19:50:49 +0500

    fix PrimeNumber

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -4,38 +4,24 @@ public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
         for (int x=2;x<=finish;x++){
-            /*int sqrt_round= (int)Math.floor(Math.sqrt(x));
+            int sqrt_round= (int)Math.floor(Math.sqrt(x));
             if (sqrt_round==1){
                 ++count;
-            }else{*/
+            }else{
                 boolean flag=true;
-                /*попробуйте упростить
-                запускаете цикл от 2 до числа внешнего цикла и если  нашли делитель то меняете флаг и делаете брейк
-                после цикла исходя из флага решаете увеличить счетчик или нет*/
                 for (int y=2;y<=x;y++){
-                    if (x%y!=0 ) {//если дошел до конца и не нашелся делитель
+                    if (x%y!=0) {
                         flag=false;
+                    }else if(x%y==0 && y<x) {
+                        flag=true;
                         break;
                     }
                 }
                 if (flag==false){
                     ++count;
                 }
-                /*for (int y=2;y<=sqrt_round;y++){
-                    if(x%y==0 && y<sqrt_round){
-                        break;
-                    }else {
-                        if (x % y == 0) {
-                            continue;
-                        } else if (y < sqrt_round) {//ладно 2 а потом будет 3 и что ?
-                            continue;
-                        } else {
-                            ++count;
-                        }
-                    }
-                }*/
             }
-        //}
+        }
         return count;
     }
 }
