0d675371a12e4c49efacbdf8e73585c9b2bfb0c2
Marat Saubanov <supper_mario@bk.ru>
2019-08-18 22:44:23 +0500

    fix Check

--- chapter_001/src/main/java/ru/job4j/array/Check.java
+++ chapter_001/src/main/java/ru/job4j/array/Check.java
@@ -2,16 +2,10 @@ package ru.job4j.array;
 
 public class Check {
     public boolean mono(boolean[] data) {
-        boolean result = false;
-            for (int x=0;x<data.length-1;x++) {
-                if (data[x]==data[x+1] && x+1<data.length-1) {
-                    result=true;
-                    continue;
-                }else if (data[x]==data[x+1] && x+1==data.length-1) {
-                    result=true;
-                    break;
-                }else {
-                    result=false;
+        boolean result = true;
+            for (int x=1;x<data.length-1;x++) {
+                if (data[0] != data[x]){
+                    result =false;
                     break;
                 }
             }
