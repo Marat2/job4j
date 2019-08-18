e8aafb9e6666927c88a11e8a9cc1f50ba646decd
Marat Saubanov <supper_mario@bk.ru>
2019-08-18 23:10:21 +0500

    EndWith

---
+++ chapter_001/src/main/java/ru/job4j/array/EndsWith.java
@@ -0,0 +1,25 @@
+package ru.job4j.array;
+
+/**
+ * Обертка над строкой.
+ */
+public class EndsWith {
+
+    /**
+     * Проверяет. что слово начинается с префикса.
+     * @param post префикс.
+     * @return если слово начинаеться с префикса
+     */
+    public boolean endsWith(String word, String post) {
+        boolean result = true;
+        char[] pst = post.toCharArray();
+        char[] wrd = word.toCharArray();
+        for (int x=0;x<pst.length;x++){
+            if (wrd[wrd.length-pst.length+x]!=pst[x]) {
+                result=false;
+                break;
+            }
+        }
+        return result;
+    }
+}
