e7c700f7664bcc729b9782fd207caa84d298c6ba
Marat Saubanov <supper_mario@bk.ru>
2019-08-21 23:26:28 +0500

    ArrayDuplicate

--- chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayDuplicate.java
@@ -2,8 +2,18 @@ package ru.job4j.array;
 import java.util.Arrays;
 
 public class ArrayDuplicate {
-    public String[] remove(String[] array){
-        String[] input = {"Привет", "Мир", "Привет", "Супер", "Мир"};
-        return input;
+    public String[] remove(String[] input){
+
+        int counter=input.length;
+        for (int x=0;x<counter;x++) {
+            String temp=input[x];
+            for (int y=x+1;y<counter;y++) {
+                if (temp.equals(input[y])) {
+                    input[y]=input[counter-1];
+                    --counter;
+                    --y;                }
+            }
+        }
+        return Arrays.copyOf(input,counter);
     }
 }
