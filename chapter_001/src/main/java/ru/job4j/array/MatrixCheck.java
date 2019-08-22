ff32547931b4926c85a6b04d6c762afcf4e54cb2
Marat Saubanov <supper_mario@bk.ru>
2019-08-22 23:01:34 +0500

    CheckStyle

--- chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
+++ chapter_001/src/main/java/ru/job4j/array/MatrixCheck.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class MatrixCheck {
     public boolean mono(boolean[][] data) {
-        boolean result = true;
+        boolean result = true; 
         for (int x=0;x<data.length;x++) {
                 if(data[x][x]!=data[0][0]){
                     result = false;
