2a9fee5ca2446146a632e5de5d778914595bd6d9
Marat Saubanov <supper_mario@bk.ru>
2019-08-31 23:39:15 +0500

    track fix
    Объединение нескольких коммитов.[#164331]

--- chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
+++ chapter_001/src/main/java/ru/job4j/loop/Mortgage.java
@@ -3,11 +3,11 @@ package ru.job4j.loop;
 public class Mortgage {
     public int year(int amount, int monthly, double percent) {
         int year = 0;
-        int perYear = monthly * 12;
-        double creditWithper = (amount + ((amount / 100) * percent));
-        while (creditWithper > 0) {
+        int Year = monthly * 12;
+        double credit = (amount + ((amount / 100) * percent));
+        while (credit > 0) {
             ++year;
-            creditWithper = creditWithper - perYear;
+            credit = credit - Year;
         }
         return year;
     }
