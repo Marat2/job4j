a3cb1e6a35241acdf14039bdd03f1ed0c6b28fb9
Marat Saubanov <supper_mario@bk.ru>
2019-08-31 21:32:42 +0500

    ArrayResult

---
+++ chapter_001/src/main/java/ru/job4j/array/ArrayResult.java
@@ -0,0 +1,17 @@
+package ru.job4j.array;
+
+public class ArrayResult {
+    public int[] create(int[] first,int[] second){
+        int result[] = new int [first.length+second.length];
+        for (int x=0,i=0;x<first.length;x++,i++) {
+            if (first[x]>second[x]) {
+                result[x+i]=second[x];
+                result[x+1+i]=first[x];
+            } else {
+                result[x+i]=first[x];
+                result[x+1+i]=second[x];
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/array/ArrayResultTest.java
@@ -0,0 +1,19 @@
+package ru.job4j.array;
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsArrayContainingInAnyOrder.arrayContainingInAnyOrder;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ArrayResultTest {
+    @Test
+    public void test() {
+        //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
+        int[] first = {1,3};
+        int[] second = {2,4};
+        int[] expect ={1,2,3,4};
+        ArrayResult array = new ArrayResult();
+        int[] result = array.create(first,second);
+        assertThat(result,is(expect));
+    }
+}
