96ac22b9c23fb508213174a9e0bc033255670424
Marat Saubanov <supper_mario@bk.ru>
2019-08-31 23:21:47 +0500

    track fix

--- chapter_001/src/test/java/ru/job4j/array/ArrayResultTest.java
+++ chapter_001/src/test/java/ru/job4j/array/ArrayResultTest.java
@@ -9,11 +9,11 @@ public class ArrayResultTest {
     @Test
     public void test() {
         //напишите здесь тест, проверяющий удаление дубликатов строк из массива строк.
-        int[] first = {1,3};
-        int[] second = {2,4};
-        int[] expect ={1,2,3,4};
+        int[] first = {1, 3};
+        int[] second = {2, 4};
+        int[] expect = {1, 2, 3, 4};
         ArrayResult array = new ArrayResult();
-        int[] result = array.create(first,second);
-        assertThat(result,is(expect));
+        int[] result = array.create(first, second);
+        assertThat(result, is(expect));
     }
 }
