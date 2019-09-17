505d41a5cc77dd6a13c1be5c6c731ce3f8fac414
Marat Saubanov <supper_mario@bk.ru>
2019-09-17 23:50:53 +0500

    StartUITEST fix

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -107,4 +107,103 @@ public class StartUITest {
                    .concat(System.lineSeparator())
                 ));
     }
+    @Test
+    public void whenUserShowItem() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("new item",  "new desc"));
+        tracker.add(new Item("new item2",  "new desc2"));
+        String CYCLE = "";
+        for (Item item:tracker.findAll()) {
+             CYCLE += "".concat(item.getName() + "  № " + item.getId()).concat(System.lineSeparator());
+        }
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()),
+                is(ADD.concat("------------ Список заявок --------------")
+                        .concat(System.lineSeparator())
+                        .concat(CYCLE)
+                        .concat("-----------------------------------------")
+                        .concat(System.lineSeparator())
+                        .concat(ADD)
+                        .concat("------------- Выход -------------")
+                        .concat(System.lineSeparator())
+                ));
+    }
+    @Test
+    public void whenUserEditItem() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("new item", "new desc"));
+        tracker.add(new Item("new item2", "new desc2"));
+        String START = "".concat("------------ Редактирование заявки --------------");
+        Item item = new Item("new item222", "new desc3232");
+        boolean result = tracker.replace(tracker.findAll()[0].getId(), item);
+        String OUTPUT;
+        if (result) {
+             OUTPUT = "".concat("------------ Заявка с getId : " + tracker.findAll()[0].getId() + "-----------").concat(System.lineSeparator())
+                  .concat("------------ Новое название : " + item.getName() + "-----------").concat(System.lineSeparator())
+                  .concat("------------ Новое описание : " + item.getDesc() + "-----------").concat(System.lineSeparator())
+                  .concat("-----------------------------------------").concat(System.lineSeparator());
+        } else {
+             OUTPUT = "".concat("------------ Ошибка замещения -----------").concat(System.lineSeparator())
+                  .concat("-----------------------------------------").concat(System.lineSeparator());
+        }
+        Input input = new StubInput(new String[]{"2", tracker.findAll()[0].getId(), "new item222", "new desc3232", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()),
+                is(ADD.concat(START).concat(System.lineSeparator()).concat(OUTPUT).concat(ADD).concat("------------- Выход -------------").concat(System.lineSeparator())));
+    }
+    @Test
+    public void whenUserDeleteItem() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("new item",  "new desc"));
+        String START = "".concat("------------ Удаление заявки --------------");
+        boolean result = true;
+        String OUTPUT;
+        if (result) {
+            OUTPUT = "".concat("------------ Заявка удалена --------------").concat(System.lineSeparator())
+                       .concat("-----------------------------------------").concat(System.lineSeparator());
+        } else {
+            OUTPUT = "".concat("------------ Ошибка удаления -----------").concat(System.lineSeparator())
+                       .concat("-----------------------------------------").concat(System.lineSeparator());
+        }
+        Input input = new StubInput(new String[]{"3", tracker.findAll()[0].getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()),
+                is(ADD.concat(START).concat(System.lineSeparator()).concat(OUTPUT).concat(ADD).concat("------------- Выход -------------").concat(System.lineSeparator())));
+    }
+    @Test
+    public void whenUserfindItemById() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("new item",  "new desc"));
+        Item item = tracker.findById(tracker.findAll()[0].getId());
+        String START = "".concat("------------ Выбрать заявку --------------");
+        String OUTPUT;
+        if (item != null) {
+            OUTPUT = "".concat("------------ Заявка с getId : " + item.getId() + "-----------").concat(System.lineSeparator())
+                       .concat("------------ Новое название : " + item.getName() + "-----------").concat(System.lineSeparator())
+                       .concat("------------ Новое описание : " + item.getDesc() + "-----------").concat(System.lineSeparator())
+                       .concat("-----------------------------------------").concat(System.lineSeparator());
+        } else {
+            OUTPUT = "".concat("----------- Элемент не найден ---------").concat(System.lineSeparator());
+        }
+        Input input = new StubInput(new String[]{"4", tracker.findAll()[0].getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()),
+                is(ADD.concat(START).concat(System.lineSeparator()).concat(OUTPUT).concat(ADD).concat("------------- Выход -------------").concat(System.lineSeparator())));
+    }
+    @Test
+    public void whenUserfindItemByName() {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("new item",  "new desc"));
+        String START = "".concat("------------ Выбрать заявку по названию--------------");
+        String CYCLE = "";
+        for (Item item:tracker.findByName("new item")) {
+            CYCLE += "".concat("№".concat(item.getId()).concat("   ").concat(item.getName())).concat(System.lineSeparator()).concat("Описание: " + item.getDesc()).concat(System.lineSeparator());
+        }
+        String END = "-----------------------------------------".concat(System.lineSeparator());
+        Input input = new StubInput(new String[]{"5", "new item", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(new String(out.toByteArray()),
+                is(ADD.concat(START).concat(System.lineSeparator()).concat(CYCLE).concat(END).concat(ADD).concat("------------- Выход -------------").concat(System.lineSeparator())));
+    }
 }
