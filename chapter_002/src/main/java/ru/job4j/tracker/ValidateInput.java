2965523e9338be2fd5e193ae77d4d2e649ee84ac
Marat Saubanov <supper_mario@bk.ru>
2019-09-28 19:36:33 +0500

    fix dekorator refactor fix

--- chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
+++ chapter_002/src/main/java/ru/job4j/tracker/ValidateInput.java
@@ -1,19 +1,20 @@
 package ru.job4j.tracker;
 
-public class ValidateInput extends ConsoleInput {
+public class ValidateInput  implements Input {
+    private final Input input;
+
+    public ValidateInput(Input input) {
+        this.input = input;
+    }
+
+    @Override
+    public String askStr(String question) {
+        return input.askStr(question);
+    }
+
     @Override
     public int askInt(String question) {
-        boolean invalid = true;
-        int value = -1;
-        do {
-            //try {
-                value = super.askInt(question);
-                invalid = false;
-           /* } catch (NumberFormatException nfe) {
-                System.out.println("Please enter validate data again ");
-            }*/
-        } while (invalid);
-        return value;
+        return 0;
     }
 
     @Override
@@ -21,15 +22,14 @@ public class ValidateInput extends ConsoleInput {
         boolean invalid = true;
         int value = -1;
         do {
-
-            //try {
-                value = super.askInt(question, max);
+            try {
+                value = input.askInt(question);
                 invalid = false;
-           /* } catch (IllegalStateException moe) {
+            } catch (IllegalStateException moe) {
                 System.out.println("Please select key from menu ");
             } catch (NumberFormatException nfe) {
                 System.out.println("Please enter validate data again ");
-            }*/
+            }
         } while (invalid);
         return value;
     }
