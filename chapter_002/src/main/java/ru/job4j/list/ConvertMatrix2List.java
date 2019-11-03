573d9cab1d4bede777821449e2ef69a337991f7f
Marat Saubanov <supper_mario@bk.ru>
2019-11-03 10:22:51 +0500

    convert matrix to list

--- chapter_002/src/main/java/ru/job4j/list/ConvertMatrix2List.java
+++ chapter_002/src/main/java/ru/job4j/list/ConvertMatrix2List.java
@@ -8,7 +8,6 @@ public class ConvertMatrix2List {
         List<Integer> list = new ArrayList<>();
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array[i].length; j++) {
-                //System.out.println(array[i][j]);
                 list.add(array[i][j]);
             }
         }
