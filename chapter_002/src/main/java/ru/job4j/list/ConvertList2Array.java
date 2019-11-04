6917b21e0a7ffc04a350baf1cd349c3931cee012
Marat Saubanov <supper_mario@bk.ru>
2019-11-04 22:06:44 +0500

    convert list to list

--- chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
+++ chapter_002/src/main/java/ru/job4j/list/ConvertList2Array.java
@@ -1,7 +1,21 @@
 package ru.job4j.list;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class ConvertList2Array {
+    public List <Integer> convert(List<int[]> list) {
+        List<Integer> result = new ArrayList<>();
+        for (int[] i:list) {
+            List<Integer> list2 = new ArrayList<Integer>(i.length);
+            for(Integer text:i) {
+                list2.add(text);
+            }
+            result.addAll(list2);
+        }
+        return result;
+    }
+
     public int[][] toArray(List<Integer> list, int rows) {
         int cells = (int) Math.ceil((double) list.size() / rows);
         int[][] array = new int[rows][cells];
