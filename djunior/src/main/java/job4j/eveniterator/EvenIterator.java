8058dc358e737531bf48dc74f1e2750d82d4b0da
Marat Saubanov <supper_mario@bk.ru>
2020-02-08 11:50:03 +0500

    second task

---
+++ djunior/src/main/java/job4j/eveniterator/EvenIterator.java
@@ -0,0 +1,38 @@
+package job4j.eveniterator;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class EvenIterator implements Iterator {
+
+    public final int[] incoming_args;
+    private int index=0;
+
+    public EvenIterator(int[] incoming_args) {
+        this.incoming_args = incoming_args;
+    }
+    //как для одного элемента сделать что бы не было в для 4 что бы было?
+    @Override
+    public boolean hasNext() {
+        //если нет четных элементов то он должен вернуть false(не трогать)
+        return incoming_args.length-1>index;
+    }
+//проверяет можно ли идти дальше , возвращает текущий эдемент(если он четный) и передвигает каретку
+    @Override
+    public Object next() {
+        if(!hasNext()){
+            throw new NoSuchElementException();
+        }
+        //--проверка четности
+        while(incoming_args[index]%2!=0){
+            index++;
+            if(!hasNext()){
+                throw new NoSuchElementException();
+            }
+        }
+        int i = incoming_args[index];
+        //--проверка четности
+        index++;
+        return  i;
+    }
+}
