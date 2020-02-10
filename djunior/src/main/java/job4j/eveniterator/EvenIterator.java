8e8bffff261198163cb4623dd97d9589697f0949
Marat Saubanov <supper_mario@bk.ru>
2020-02-10 21:41:18 +0500

    second task

--- djunior/src/main/java/job4j/eveniterator/EvenIterator.java
+++ djunior/src/main/java/job4j/eveniterator/EvenIterator.java
@@ -12,20 +12,18 @@ public class EvenIterator implements Iterator {
         this.incoming_args = incoming_args;
     }
     @Override
-    //2 случая  - если последний элемент четный и если последний элемент не четный. (2,4,6,8 и 1,2,3,4,5,6,7)
     public boolean hasNext() {
         boolean result = false;
-        int i = this.index;
         if (incoming_args.length<=index || incoming_args.length==1) {
             result = false;
         }else {
-            while (incoming_args.length<=i || incoming_args[i]%2!=0) {//пребавляю пока не найду четный или не выйду за предел
-                i++;
-                if(i>incoming_args.length){
+            while (incoming_args.length<=this.index || incoming_args[this.index]%2!=0) {
+                this.index++;
+                if(this.index>incoming_args.length){
                     throw new NoSuchElementException();
                 }
             }
-            if (i>=incoming_args.length) {
+            if (this.index>=incoming_args.length) {
                 result = false;
             } else {
                 result = true;
@@ -34,21 +32,12 @@ public class EvenIterator implements Iterator {
         }
         return result;
     }
-//проверяет можно ли идти дальше , возвращает текущий эдемент(если он четный) и передвигает каретку
     @Override
     public Object next() {
         if(!hasNext()){
             throw new NoSuchElementException();
         }
-        //--проверка четности
-        while(incoming_args[index]%2!=0){
-            index++;
-            if(!hasNext()){
-                throw new NoSuchElementException();
-            }
-        }
         int i = incoming_args[index];
-        //--проверка четности
         index++;
         return  i;
     }
