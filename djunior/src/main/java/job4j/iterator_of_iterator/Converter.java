706d11eb77eb6b133ed757cac21fb165cae7c206
Marat Saubanov <supper_mario@bk.ru>
2020-02-13 22:14:00 +0500

    third task

---
+++ djunior/src/main/java/job4j/iterator_of_iterator/Converter.java
@@ -0,0 +1,42 @@
+package job4j.iterator_of_iterator;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+        return new Iterator<Integer>() {
+            private Iterator<Integer> inner = it.next();
+            @Override
+            public boolean hasNext() {
+                boolean result = false;
+                if (inner.hasNext()){
+                    result = true;
+                }else {
+                    if (it.hasNext()){
+                        inner=it.next();
+                        result = true;
+                        while (!inner.hasNext() && it.hasNext()){
+                            it.next();
+                            result = false;
+                        }
+                    }else {
+                        result=false;
+                    }
+                }
+                return result;
+            }
+
+            @Override
+            public Integer next() {
+                if (!hasNext()) {
+                    throw new NoSuchElementException();
+                }
+                Integer i = inner.next();
+                return i;
+            }
+        };
+    }
+}
