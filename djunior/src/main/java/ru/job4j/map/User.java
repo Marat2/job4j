db09ad2ffec93ac4028eb0d12fed7cda3cd60cc4
Marat Saubanov <supper_mario@bk.ru>
2020-03-28 15:43:04 +0500

    first map

--- djunior/src/main/java/ru/job4j/map/User.java
+++ djunior/src/main/java/ru/job4j/map/User.java
@@ -7,5 +7,14 @@ public class User {
     private String name;
     private int children;
     private Calendar birthday;
-    
+    public User(String name, int children, Calendar birthday){
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    /*@Override
+    public String toString() {
+        return " Name: "+this.name+" children: "+this.children+" birthday: "+this.birthday.getWeekYear();
+    }*/
 }
